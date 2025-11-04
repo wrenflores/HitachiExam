178ead9c3b1fcbd5fd46cc3d195d0c750d72648e
Wren Jomille flores <wrenflores@Wrens-MacBook-Pro.local>
2025-11-04 20:42:53 +0800

    Initial Commit

---
+++ src/test/java/com/smartpark/parkingmanagementsystem/ParkingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartpark.parkingmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
